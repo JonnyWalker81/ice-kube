d1ffb08ecd398c9bf2a20fff3f911fd37ef3b028
Jonathan Rothberg <jonnyrpi@gmail.com>
2020-07-21 00:48:53 -0700

    Allow for streaming multiple pods at the same time.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@ use kube::{
     Client, Config,
 };
 // use log::info;
+use futures::future::join_all;
+use regex::Regex;
 use std::collections::HashMap;
 use std::io;
 use std::io::Write;
 use termion::{color, style};
+use tokio::task;
 
 #[derive(Clap)]
 #[clap(version = "0.1.0", author = "Jonathan Rothberg")]
@@ -35,6 +38,8 @@ struct LogsOpts {
     namespace: String,
     #[clap(short = 't', long = "tail-length", default_value = "100")]
     tail_length: i64,
+    #[clap(short = 'p', long = "pattern")]
+    pattern: Option<String>,
 }
 
 #[tokio::main]
@@ -44,23 +49,40 @@ async fn main() -> Result<()> {
     match opts.subcmd {
         SubCmd::Logs(o) => match o.pod {
             Some(p) => {
-                stream_logs(&o.namespace, &p, o.tail_length).await?;
+                stream_logs(o.namespace, p, o.tail_length).await?;
             }
             None => {
-                // println!("Ops: {:?}", o);
-                let pods = list_pods(&o.namespace).await?;
-                let mut input = String::new();
-                print!("Enter Pod number: ");
-                io::stdout().flush()?;
-                match io::stdin().read_line(&mut input) {
-                    Ok(_) => {
-                        let index = input.trim().parse::<usize>()?;
-                        if let Some(p) = pods.get(&index) {
-                            println!("{}", p);
-                            stream_logs(&o.namespace, &p, o.tail_length).await?;
+                match o.pattern {
+                    Some(p) => {
+                        let pods = collect_pods(&o.namespace, &p).await?;
+                        println!("Pods: {:?}", pods);
+                        let mut tasks = vec![];
+                        for name in pods {
+                            let n = o.namespace.clone();
+                            let pn = name.clone();
+                            let t = task::spawn(stream_logs(n, pn, o.tail_length));
+                            tasks.push(t);
+                        }
+
+                        join_all(tasks).await;
+                    }
+                    None => {
+                        // println!("Ops: {:?}", o);
+                        let pods = list_pods(&o.namespace).await?;
+                        let mut input = String::new();
+                        print!("Enter Pod number: ");
+                        io::stdout().flush()?;
+                        match io::stdin().read_line(&mut input) {
+                            Ok(_) => {
+                                let index = input.trim().parse::<usize>()?;
+                                if let Some(p) = pods.get(&index) {
+                                    println!("{}", p);
+                                    stream_logs(o.namespace, p.clone(), o.tail_length).await?;
+                                }
+                            }
+                            Err(error) => println!("error: {}", error),
                         }
                     }
-                    Err(error) => println!("error: {}", error),
                 }
             }
         },
@@ -73,7 +95,8 @@ async fn list_pods(namespace: &str) -> Result<HashMap<usize, String>> {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
     let mut client_config = Config::infer().await?;
-    client_config.timeout = std::time::Duration::from_secs(60 * 60);
+    // client_config.timeout = std::time::Duration::from_secs(60 * 60 * 24);
+    client_config.timeout = None;
     let client = Client::new(client_config);
 
     let pods: Api<Pod> = Api::namespaced(client, namespace);
@@ -88,30 +111,56 @@ async fn list_pods(namespace: &str) -> Result<HashMap<usize, String>> {
     Ok(pod_map)
 }
 
-async fn stream_logs(namespace: &str, pod_name: &str, tail_lines: i64) -> Result<()> {
+async fn collect_pods(namespace: &str, pattern: &str) -> Result<Vec<String>> {
     let mut client_config = Config::infer().await?;
-    client_config.timeout = std::time::Duration::from_secs(60 * 60);
+    // client_config.timeout = std::time::Duration::from_secs(60 * 60 * 24);
+    client_config.timeout = None;
     let client = Client::new(client_config);
 
     let pods: Api<Pod> = Api::namespaced(client, namespace);
+    let mut lp = ListParams::default();
+    lp.timeout = None;
+    let mut matching_pods = vec![];
+    let re: Regex = Regex::new(pattern).unwrap();
+    for (_, p) in (pods.list(&lp).await?).into_iter().enumerate() {
+        // lazy_static! {
+        //     static ref RE: Regex = Regex::new(pattern).unwrap();
+        // }
+        let name = Meta::name(&p).to_string();
+        if re.is_match(&name) {
+            matching_pods.push(name);
+        }
+    }
+
+    Ok(matching_pods)
+}
+
+async fn stream_logs(namespace: String, pod_name: String, tail_lines: i64) -> Result<()> {
+    let mut client_config = Config::infer().await?;
+    // client_config.timeout = std::time::Duration::from_secs(60 * 60 * 24);
+    client_config.timeout = None;
+    let client = Client::new(client_config);
+
+    let pods: Api<Pod> = Api::namespaced(client, &namespace);
     let mut lp = LogParams::default();
     lp.follow = true;
     lp.pretty = true;
     lp.tail_lines = Some(tail_lines);
-    let mut logs = pods.log_stream(pod_name, &lp).await?.boxed();
-    println!("{}Test", color::Fg(color::White));
+    let mut logs = pods.log_stream(&pod_name, &lp).await?.boxed();
+    println!("{}", color::Fg(color::White));
     while let Some(line) = logs.try_next().await? {
         let line_str = String::from_utf8((&line).to_vec())?;
         if line_str.contains("ERROR") || line_str.contains("error") {
             println!(
-                "{}{}{}{}",
+                "{}  {}{}{}{}",
+                pod_name,
                 color::Fg(color::Red),
                 style::Bold,
                 line_str,
                 style::Reset
             );
         } else {
-            println!("{}{}", color::Fg(color::White), line_str);
+            println!("{}  {}{}", pod_name, color::Fg(color::White), line_str);
         }
     }
 
